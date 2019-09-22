c46560db60624afba277ebc220c613bf4a5d547e
Prateek kumar <prateek2109@outlook.com>
2019-09-23 02:40:13 +0530

    Part 4(App level Scope and Activity level Scope and Dependency)

--- app/src/main/java/com/example/daggerintro/AppComponent.java
+++ app/src/main/java/com/example/daggerintro/AppComponent.java
@@ -10,6 +10,8 @@ import dagger.Component;
 @Component(modules = {DriverModule.class})
 public interface AppComponent {
 
+    // If we have other Classes in this Component and if we have not Exposed them explicetely then
+    //Depedency will not be able to access it
 
     Driver getDriver();
 
